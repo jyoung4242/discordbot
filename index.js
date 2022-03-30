2bd11c44993ee3d2373ac99899f0a9715e83743d
Justin Young <justin_dean_young@yahoo.com>
2022-03-30 13:46:02 -0400

    comments

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+//defined constancts that need adjusted for each channel
 const MEESAGELIMIT = 25;
 const SLOWMODERESETLIMIT = 5;
 const SLOWMODERATE = 20;
 const COOLDOWNPERIOD = 4000;
 const channelID = '958704435117293621';
 
+//defined values, the message queu and the flag
 let messageArry = [];
 let slowDownActive = false;
 
@@ -16,11 +18,16 @@ const client = new DiscordJS.Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
+//Hello World
 client.on('ready', message => {
     console.log(`BOT ready!`);
     client.channels.cache.get(channelID).send('spammyBot Here: Well hello there!');
 });
 
+//on messages that come from someone other than the bot
+//and its from the monitored channel
+//adds message to queu and it queue exceeds maximum value,
+//induces slow mode until number of queued messages dips below threshhold
 client.on('messageCreate', msg => {
     if (msg.author.username != 'spammyBot' && msg.channelId == channelID) {
         console.log(`Message added to messagequeue`);
@@ -33,6 +40,8 @@ client.on('messageCreate', msg => {
     }
 });
 
+//interval process through periodically removes old message off queu
+//the iteration rate is configurable by the constant at top of file
 setInterval(() => {
     if (messageArry.length) {
         messageArry.pop();
